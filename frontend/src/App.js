ab68e86b3c12ae7c72cab9c5cc561173d8ba4c7a
Yaroslav Shakalov <yaroslavshakalov888@gmail.com>
2022-05-13 18:17:32 +0300

    get rid of useless files and add internationalization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { useTranslation } from 'react-i18next';
+
 import { login } from './utils/utils';
+
+import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const [ t ] = useTranslation();
 
   if ( !window.walletConnection.isSignedIn() ) {
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
           Learn React
-        </a>
-        <button onClick={login}> Login </button>
-      </header>
-    </div>
-    )
+          </a>
+          <button onClick={ login }> { t( 'LOGIN_BUTTON' ) } </button>
+        </header>
+      </div>
+    );
   }
-}
+};
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import { initContract } from './utils/utils';
+
+import './utils/i18n';
+import './index.css';
 
 window.nearInitPromise = initContract()
   .then( () => {
@@ -11,13 +14,7 @@ window.nearInitPromise = initContract()
 
     root.render(
       <React.StrictMode>
-      <App />
+          <App />
       </React.StrictMode>
     );
   } ).catch( console.error );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
