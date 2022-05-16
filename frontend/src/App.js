c181bc892b3ebfc98cbc58355308b34d8205506e
Yaroslav Shakalov <yaroslavshakalov888@gmail.com>
2022-05-16 14:23:01 +0300

    configure eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
-import { login } from './utils/utils';
+import { login, logout } from './utils/utils';
 
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   const [ t ] = useTranslation();
 
-  if ( !window.walletConnection.isSignedIn() ) {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Learn React
-          </a>
-          <button onClick={ login }> { t( 'LOGIN_BUTTON' ) } </button>
-        </header>
-      </div>
-    );
-  }
+  const [ number, setNumber ] = useState();
+
+  const isSignedIn = window.walletConnection.isSignedIn();
+
+  useEffect( () => {
+    if ( isSignedIn ) {
+      window.contract.get_num()
+        .then( ( num ) => setNumber( num ) )
+        .catch( console.log );
+    }
+  }, [] );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {
+          !isSignedIn
+            ? <button
+              onClick={ login }>
+              { t( 'LOGIN_BUTTON' ) }
+            </button>
+            : <button onClick={ () => logout }> { t( 'LOGOUT_BUTTON' ) } { number } </button>
+        }
+      </header>
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,28 +1,35 @@
-import { connect, Contract, keyStores, WalletConnection } from 'near-api-js'
-import getConfig from "../config/near_config";
+import { connect, Contract, keyStores, WalletConnection } from 'near-api-js';
+import getConfig from '../config/near_config';
 
-const nearConfig = getConfig( process.env.REACT_APP_NODE_ENV || 'development' )
+const nearConfig = getConfig( process.env.REACT_APP_NODE_ENV || 'development' );
 
 const initContract = async () => {
   const near = await connect( Object.assign(
-    { deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } },
+    { deps : { keyStore : new keyStores.BrowserLocalStorageKeyStore() } },
     nearConfig,
   ) );
 
   window.walletConnection = new WalletConnection( near );
   window.accountId = window.walletConnection.getAccountId();
+  console.log( window.walletConnection.account() );
   window.contract = await new Contract(
     window.walletConnection.account(),
-    nearConfig.contractName,
-    { viewMethods: [ 'get_greeting' ], changeMethods: [ 'set_greeting' ] },
+    window.accountId,
+    { viewMethods : [ 'get_num' ], changeMethods : [ 'increment' ] },
   );
-}
+};
 
 const login = () => {
-  window.walletConnection.requestSignIn(nearConfig.contractName);
-}
+  window.walletConnection.requestSignIn( nearConfig.contractName );
+};
+
+const logout = () => {
+  window.walletConnection.signOut();
+  window.location.replace( window.location.origin + window.location.pathname );
+};
 
 export {
-  initContract,
   login,
-}
+  initContract,
+  logout,
+};
